d2ab0684e40f03354245c51e1c6640e703ce683f
Brett Jory <bjory7@gmail.com>
2019-05-31 10:05:03 -0700

    finished assignment Fundamentals I

---
+++ lang_essentials/fundamentalsI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fundamentalsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+					int i = 1;
+					while (i <= 255) {
+						Console.WriteLine(i);
+						i++;
+					}
+
+					int j = 1;
+
+					while (j <= 100) {
+						if ( j % 5 == 0 && j % 3 == 0 ) {
+							Console.WriteLine("FizzBuzz");
+						} else if (j % 3 == 0) {
+							Console.WriteLine("Fizz");
+						} else if (j % 5 == 0) {
+							Console.WriteLine("Buzz");
+						}
+						j++;
+					}
+
+
+        }
+    }
+}
