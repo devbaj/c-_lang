72fae7b004bf16be40f1529218e3c40aacc1a0f6
Brett Jory <bjory7@gmail.com>
2019-06-01 12:52:44 -0700

    finished assignment Human

--- oop/human/Program.cs
+++ oop/human/Program.cs
@@ -2,6 +2,48 @@
 
 namespace human
 {
+
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+        }
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Dexterity = 3;
+            Intelligence = 3;
+            health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int setHealth)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            health = setHealth;
+        }
+
+        public int Attack(Human target)
+        {
+            target.health -= (this.Strength) * 5;
+            return target.health;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
